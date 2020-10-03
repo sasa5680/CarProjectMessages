706f6f51400c47f79b07e7ed91e9b04c822e6fc3
sasa5680 <sasa5680@naver.com>
2020-10-03 17:59:57 +0900

    Initial commit

---
+++ src/main/java/com/sasa5680/Index/Net.java
@@ -0,0 +1,7 @@
+package com.sasa5680.Index;
+
+public class Net {
+	
+	public static final int Port = 8008;
+
+}
---
+++ src/main/java/com/sasa5680/ProtoMessages/MessageWrapper.java
@@ -0,0 +1,18 @@
+package com.sasa5680.ProtoMessages;
+import com.google.protobuf.Any;
+
+public class MessageWrapper {
+	
+	
+	/*
+	public static General Wrap_Message(com.google.protobuf.Message inner) {
+		
+		Any any = Any.pack(inner);
+		General msg = General.newBuilder()
+							 .addInnerMSG(any)
+							 .setMessageType(inner.getClass().getSimpleName())
+							 .build();
+		return msg;
+	}
+	*/
+}
