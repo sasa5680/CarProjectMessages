af743ee8c3effc20e1497210add8be0dd9d21328
sasa5680 <sasa5680@naver.com>
2020-10-04 17:22:14 +0900

    dd

--- src/main/java/com/sasa5680/ProtoMessages/MessageWrapper.java
+++ src/main/java/com/sasa5680/ProtoMessages/MessageWrapper.java
@@ -1,10 +1,11 @@
 package com.sasa5680.ProtoMessages;
 import com.google.protobuf.Any;
+import com.sasa5680.ProtoMessages.GeneralOuterClass.General;
 
 public class MessageWrapper {
 	
 	
-	/*
+	
 	public static General Wrap_Message(com.google.protobuf.Message inner) {
 		
 		Any any = Any.pack(inner);
@@ -14,5 +15,5 @@ public class MessageWrapper {
 							 .build();
 		return msg;
 	}
-	*/
+	
 }
